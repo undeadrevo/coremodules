6ad847cc6612c1f12d2f6797f6fd8f5c6c649b79
ApolloJustice <me@apollojustice.me>
2015-06-06 20:31:12 -0500

    first commit

---
+++ niggr.py
@@ -0,0 +1,18 @@
+import requests
+from util.hook import *
+from util import output
+
+apiurl = 'http:/nig.gr/api/'
+
+
+@hook(cmds=['short', 'nig', 'bitly'], ex='shorten url', rate=10, args=True)
+def search(code, input):
+
+    try:
+        url = input.group(2)
+        niggurl = requests.get(apiurl + url)
+
+        return code.say('Shortened URL: %s' % niggurl)
+    except Exception as e:
+        output.error('Error in nigurl.py: %s' % str(e))
+        return code.say('{b}Unable to shorten %s{b}' % input.group(2))
